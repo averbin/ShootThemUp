542dc05afd1dfeecfea023b8f5f70799739dfba3
averbin <alexander.verbin@swissmeda.com>
2024-03-04 18:12:19 +0200

    104 AI EQS Context

--- Source/ShootThemUp/ShootThemUp.Build.cs
+++ Source/ShootThemUp/ShootThemUp.Build.cs
@@ -30,7 +30,8 @@ public class ShootThemUp : ModuleRules
 			"ShootThemUp/Public/Animations",
 			"ShootThemUp/Public/Pickups",
             "ShootThemUp/Public/AI",
-			"ShootThemUp/Public/AI/Tasks",
+            "ShootThemUp/Public/AI/EQS",
+            "ShootThemUp/Public/AI/Tasks",
 			"ShootThemUp/Public/AI/Services"});
 
 		// Uncomment if you are using Slate UI
